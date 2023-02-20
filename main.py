1e58230635daec6dc78c384d8694affa29e5236a
muhammadharoun <hro19502001@gmail.com>
2023-02-20 10:17:35 +0200

    fix pylint

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 print('this is a test')
+
+
 # Path: test-pylint\test.py
 # function to create csv file
-def Create_csv():
+def create_csv():
     print('this is a test')
     return
+
+
 # function to read csv file
 def read_csv():
     print(' this is a test')
     return
+
+
 # function to update csv file
 def update_csv():
     print('this is a test')
