c413dd427632e89f5d2d709ee3f0b6773d864712
muhammadharoun <hro19502001@gmail.com>
2023-02-20 10:13:45 +0200

    test

---
+++ main.py
@@ -0,0 +1,14 @@
+print('this is a test')
+# Path: test-pylint\test.py
+# function to create csv file
+def Create_csv():
+    print('this is a test')
+    return
+# function to read csv file
+def read_csv():
+    print(' this is a test')
+    return
+# function to update csv file
+def update_csv():
+    print('this is a test')
+    return
